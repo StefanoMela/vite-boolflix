3d1b2fb88da02cd3158c5bca9ee416fdf169716d
StefanoMela <stefano.mela25@gmail.com>
2023-09-17 18:59:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+
+
+export default {
+  data() {
+    return {
+      title: "Hello world"
+    }
+  }
+};
+</script>
+
+<template>
+  <h1 class="text-warning text-center">{{ title }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+  data() {
+    return {
+      title: "Hello world"
+    }
+  }
+};
+</script>
+
+<template>
+  <h3>Sono il main</h3>
+</template>
+
+<style lang="scss" scoped></style>
